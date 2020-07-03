1b2b53fe4ba511d0620ed85af104ffe4595c6623
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2020-07-03 09:58:12 +0200

    add checklist_template()

---
+++ R/checklist_template.R
@@ -0,0 +1,27 @@
+#' Create a template with acceptable problems in the package
+#'
+#' @param output The output of `check_package()`.
+#' @inheritParams check_package
+#' @importFrom assertthat assert_that has_name
+#' @importFrom yaml write_yaml
+#' @export
+checklist_template <- function(output, path = ".") {
+  assert_that(is.list(output))
+  assert_that(has_name(output, "warnings"))
+  assert_that(is.character(output$warnings))
+  assert_that(has_name(output, "notes"))
+  assert_that(is.character(output$notes))
+
+  key_value <- function(x) {
+    list(motivation = "", value = x)
+  }
+  list(
+    description = "Configuration file for checklist::check_pkg()",
+    allowed = list(
+      warnings = lapply(output$warnings, key_value),
+      checks = lapply(output$notes, key_value)
+    )
+  ) -> template
+  write_yaml(template, file.path(path, "checklist_template.yml"))
+  return(NULL)
+}
