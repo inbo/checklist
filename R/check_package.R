7285e8d1149c91090302651571f7af45e0f2573f
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2020-07-07 11:22:41 +0200

    Change the default for the fail argument of check_package()
    The new default depends on the status of the session.
    - `FALSE` for interactive sessions, not triggering an error in case of failure.
    - `TRUE` for non-interactive sessions, triggering an error in case of failure.

--- R/check_package.R
+++ R/check_package.R
@@ -2,11 +2,12 @@
 #'
 #' @inheritParams read_checklist
 #' @param fail Should the function return an error in case of a problem?
-#' Defaults to `TRUE`.
+#' Defaults to `TRUE` on non-interactive session and `FALSE` on an interactive
+#' session.
 #' @importFrom assertthat assert_that is.flag is.string noNA
 #' @importFrom utils file_test
 #' @export
-check_package <- function(x = ".", fail = TRUE) {
+check_package <- function(x = ".", fail = !interactive()) {
   x <- check_cran(x = x)
 
   cat("Checking code style\n")
