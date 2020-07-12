922021db635e4e323437ea2da98cbaa0992cb830
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2020-07-12 13:44:40 +0200

    Improve check_filename()
    - on a git repo with changes in the index: check only those files
    - on a git repo with a clean workspace: check all files known to git
    - bugifx in selecting folders
    - bugfix with empty ignore files

--- R/check_filename.R
+++ R/check_filename.R
@@ -1,17 +1,36 @@
 #' Check the style of file names
 #' @inheritParams read_checklist
 #' @export
+#' @importFrom git2r in_repository ls_tree
 check_filename <- function(x = ".") {
   if (!inherits(x, "Checklist") || !"checklist" %in% x$get_checked) {
     x <- read_checklist(x = x)
   }
 
-  dirs <- list.dirs(x$get_path, recursive = TRUE, full.names = FALSE)
+  if (in_repository(x$get_path)) {
+    repo <- repository(x$get_path)
+    files <- unlist(status(repo, untracked = FALSE))
+    if (length(files) == 0) {
+      files <- ls_tree(repo = repo)
+      dirs <- unique(files$path)
+      files <- paste0(files$path, files$name)
+      dirs <- gsub("/$", "", dirs)
+    } else {
+      dirs <- unique(dirname(files))
+      dirs <- dirs[dirs != "."]
+    }
+  } else {
+    dirs <- list.dirs(x$get_path, recursive = TRUE, full.names = FALSE)
+    files <- list.files(x$get_path, recursive = TRUE, all.files = TRUE)
+  }
+
   # ignore git and RStudio files
   dirs <- dirs[!grepl("\\.(git|Rproj.user)(/.*)?$", dirs)]
-  ok_dirs <- c("", "R")
+  ok_dirs <- c(
+    "", "R", ".github/ISSUE_TEMPLATE", ".github/PULL_REQUEST_TEMPLATE"
+  )
   dirs <- dirs[!dirs %in% c(ok_dirs)]
-  check_dir <- grepl("^\\.?([a-z0-9_])+", dirs)
+  check_dir <- grepl("^\\.?([a-z0-9_\\/])+$", dirs)
   problems <- sprintf(
 "Folder names should only contain lower case letters, numbers and underscore.
 They can start with a single dot.
@@ -19,9 +38,8 @@ Failing folder: `%s`",
     dirs[!check_dir]
   )
 
-  files <- list.files(x$get_path, recursive = TRUE, all.files = TRUE)
   # ignore git and RStudio files
-  files <- files[-grep("\\.(git|Rproj.user)/.*", files)]
+  files <- files[!grepl("\\.(git|Rproj.user)/.*", files)]
   # ignore some standardised files
   re <- sprintf(
     "^(%s)$",
@@ -44,7 +62,7 @@ Failing folder: `%s`",
   )
 
   extension <- gsub("(.*)\\.(.*)?", "\\2", basename(files))
-  # extention exceptions
+  # extension exceptions
   exception <- grepl("^R(proj|d|md|nw)?$", extension)
   problems <- c(
     problems,
