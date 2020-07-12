6dd14fb652a2c354772426d63fa3a0cb5bbd7521
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2020-07-12 15:08:02 +0200

    Fix unit test
    - use normalizePath()

--- tests/testthat/test_a_read_checklist.R
+++ tests/testthat/test_a_read_checklist.R
@@ -4,7 +4,7 @@ test_that("read_checklist works", {
   # no checklist.yml
   expect_is(x <- read_checklist(target), "Checklist")
   expect_identical(read_checklist(x), x)
-  expect_identical(x$get_path, target)
+  expect_identical(x$get_path, normalizePath(target, winslash = "/"))
   expect_identical(x$get_checked, "checklist")
   expect_is(x$.__enclos_env__$private$allowed_notes, "list")
   expect_is(x$.__enclos_env__$private$allowed_warnings, "list")
