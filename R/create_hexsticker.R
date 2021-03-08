4f0489caca8fae914e3fa9ec40f5cedb15e81566
ElsLommelen <els.lommelen@inbo.be>
2021-03-08 14:12:32 +0100

    add function create_hexsticker()

---
+++ R/create_hexsticker.R
@@ -0,0 +1,29 @@
+#' @title Make hexagonal logo for package
+#'
+#' @description This function makes a hexsticker in INBO style for the provided
+#' package name.
+#'
+#' @param package_name package name that should be mentioned on the hexsticker
+#' @param filename filename to save the sticker
+#'
+#' @return A figure is saved in the working directory or provided path
+#'
+#' @export
+#'
+#' @importFrom hexSticker sticker
+#'
+#' @family utils
+#'
+#' @examples
+#' create_hexsticker("checklist", filename = "hexsticker.png")
+
+create_hexsticker <- function(package_name, filename = "hexsticker.png") {
+  background <- system.file("inbo-empty.png", package = "checklist")
+  sticker(
+    background, s_x = 1, s_y = 1, s_width = 1, s_height = 1, asp = 0.85,
+    package = package_name, p_y = 0.75, p_color = "#000000",
+    p_family = "Flanders Art Sans", p_size = 24, p_fontface = "bold",
+    h_color = "#c04384", filename = filename
+  )
+}
+
