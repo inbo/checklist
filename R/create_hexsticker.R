bd768adbf3a095cb4b1abe36a2304221494470a6
ElsLommelen <els.lommelen@inbo.be>
2021-03-09 13:44:37 +0100

    replace require with requireNamespace

--- R/create_hexsticker.R
+++ R/create_hexsticker.R
@@ -17,10 +17,10 @@
 
 create_hexsticker <-
   function(package_name, filename = "man/figures/hexsticker.png") {
-  if (require("hexSticker") & require(showtext)) {
+  if (requireNamespace("hexSticker") & requireNamespace(showtext)) {
     background <- system.file("inbo-empty.png", package = "checklist")
-    font_add(family = "Flanders Art Sans", regular = "FlandersArtSans-Medium.otf")
-    sticker(
+    showtext::font_add(family = "Flanders Art Sans", regular = "FlandersArtSans-Medium.otf")
+    hexSticker::sticker(
       background, s_x = 1, s_y = 1, s_width = 1, s_height = 1, asp = 0.85,
       package = package_name, p_y = 0.75, p_color = "#000000",
       p_family = "Flanders Art Sans", p_size = 24,
