3197b6680020928bdcebd70e92c7d201c04e41cd
ElsLommelen <els.lommelen@inbo.be>
2021-03-10 12:07:11 +0100

    add font to package (and adapt code accordingly)

--- R/create_hexsticker.R
+++ R/create_hexsticker.R
@@ -17,13 +17,15 @@
 
 create_hexsticker <-
   function(package_name, filename = "man/figures/hexsticker.png") {
-  if (requireNamespace("hexSticker") & requireNamespace("extrafont") &
+  if (requireNamespace("hexSticker") & requireNamespace("showtext") &
       requireNamespace("sysfonts")) {
     background <- system.file("inbo-empty.png", package = "checklist")
-    if ("FlandersArtSans-Medium" %in% extrafont::fonts()) {
-      sysfonts::font_add(family = "Flanders Art Sans",
-                         regular = "FlandersArtSans-Medium.otf")
-    }
+    showtext::showtext_auto()
+    sysfonts::font_add(
+      family = "Flanders Art Sans",
+      regular =
+        system.file("fonts/flanders_art_sans_medium.ttf", package = "checklist")
+    )
     hexSticker::sticker(
       background, s_x = 1, s_y = 1, s_width = 1, s_height = 1, asp = 0.85,
       package = package_name, p_y = 0.75, p_color = "#000000",
