01ba79c337ec0c335f75eaa32e8bfcbcd3b4c32f
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-08-25 11:48:06 +0200

    push tags via entrypoint

--- docker/entrypoint_package.sh
+++ docker/entrypoint_package.sh
@@ -48,32 +48,33 @@ echo 'Event name:' $GITHUB_EVENT_NAME
 echo 'ref:' $GITHUB_REF
 if [ "$GITHUB_ACTIONS" != "true" ]; then
   echo '\nNot updating tag, because not a GitHub action.';
-elif [ "$GITHUB_EVENT_NAME" != "push" ]; then
-  echo '\nNot updating tag, because not a push event.';
-elif [ "$GITHUB_REF" != "refs/heads/main" ] && [ "$GITHUB_REF" != "refs/heads/master" ]; then
-  echo '\nNot updating tag, because not on main or master.';
-else
-  echo '\nUpdating tag...\n';
-  if [ "$GITHUB_REF" == "refs/heads/main" ]; then
-    git checkout main
-  else
-    git checkout master
-  fi
-  Rscript --no-save --no-restore -e 'checklist::set_tag()';
-
-  echo '\nPush pkgdown website...\n'
-  cp -R docs ../docs
-  if [ -z  "$(git branch -r | grep origin/gh-pages)" ]; then
-    git checkout --orphan gh-pages
-    git rm -rf --quiet .
-    git commit --allow-empty -m "Initializing gh-pages branch"
-  else
-    git checkout -b gh-pages
-    git rm -rf --quiet .
-    rm -R *
-  fi
-  cp -R ../docs/. .
-  git add --all
-  git commit --amend -m "Automated update of gh-pages website"
-  git push --force --set-upstream origin gh-pages
+  exit 0
 fi
+if [ "$GITHUB_EVENT_NAME" != "push" ]; then
+  echo '\nNot updating tag, because not a push event.';
+  exit 0
+fi
+if [ "$GITHUB_REF" != "refs/heads/main" ] && [ "$GITHUB_REF" != "refs/heads/master" ]; then
+  echo '\nNot updating tag, because not on main or master.';
+  exit 0
+fi
+echo '\nUpdating tag...\n';
+git rev-parse --abbrev-ref origin/HEAD | sed 's/origin\///' | xargs git checkout
+Rscript --no-save --no-restore -e 'checklist::set_tag()';
+git push --follow-tags
+
+echo '\nPush pkgdown website...\n'
+cp -R docs ../docs
+if [ -z  "$(git branch -r | grep origin/gh-pages)" ]; then
+  git checkout --orphan gh-pages
+  git rm -rf --quiet .
+  git commit --allow-empty -m "Initializing gh-pages branch"
+else
+  git checkout -b gh-pages
+  git rm -rf --quiet .
+  rm -R *
+fi
+cp -R ../docs/. .
+git add --all
+git commit --amend -m "Automated update of gh-pages website"
+git push --force --set-upstream origin gh-pages
