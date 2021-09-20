bb16bd077c30b91d6e005fa1d6c7d7916b1b51bd
hansvancalster <hans.vancalster@inbo.be>
2021-09-20 16:29:02 +0200

    add function is_repository

--- R/utils.R
+++ R/utils.R
@@ -246,3 +246,18 @@ quiet_cat <- function(x, quiet = FALSE, ...) {
     cat(x, ...)
   }
 }
+
+#' Determine if a directory is in a git repository
+#'
+#' The path arguments specifies the directory at which to start the search for
+#' a git repository.
+#' If it is not a git repository itself, then its parent directory is consulted,
+#' then the parent's parent, and so on.
+#' @inheritParams gert::git_find
+#' @importFrom gert git_find
+#' @return TRUE if directory is in a git repository else FALSE
+#' @export
+is_repository <- function(path = ".") {
+  out <- tryCatch(git_find(path = path), error = function(e) e)
+  !any(class(out) == "error")
+}
