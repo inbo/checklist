98a4d13b7a9bc9af25bfae143d3b8dff1ef1a375
hansvancalster <hans.vancalster@inbo.be>
2021-09-20 11:08:39 +0200

    try to set initial branch to main

--- tests/testthat/test_c_new_branch.R
+++ tests/testthat/test_c_new_branch.R
@@ -7,6 +7,8 @@ test_that("new_branch() creates a branch from the main branch", {
   dir.create(path)
   on.exit(unlink(path, recursive = TRUE), add = TRUE)
 
+  gert::git_config_global_set(name = "init.defaultBranch", value = "main")
+
   origin_repo <- gert::git_init(path = origin_path, bare = TRUE)
   repo <- gert::git_clone(url = origin_path, path = path, verbose = FALSE)
 
