85f8b82775495b204a01dcf6b1b456d156e8ff1a
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-11-19 10:40:15 +0100

    check_source() on GHA handles projects with renv

--- docker/entrypoint_source.sh
+++ docker/entrypoint_source.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 
 echo '\nGetting the code...\n'
-git clone https://$INPUT_TOKEN@github.com/$INPUT_REPOSITORY check
+git clone https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY check
 cd check
 git config advice.detachedHead false
 git checkout $GITHUB_SHA
-cd $INPUT_PATH
+cd ..
 
-Rscript --no-save --no-restore -e 'checklist::check_source()'
+Rscript --no-save --no-restore -e 'checklist::check_source("check/'$INPUT_PATH'")'
 if [ $? -ne 0 ]; then
   echo '\nThe source code failed some checks. Please check the error message above.\n';
   exit 1
