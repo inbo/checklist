4c1dcc241aa3bf2f22977895e7a98fdf97bc576d
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-11-29 21:04:16 +0100

    reproduce bug in check_description() when checking version numbers

--- tests/testthat/test_d_check_description.R
+++ tests/testthat/test_d_check_description.R
@@ -40,18 +40,49 @@ test_that("check_description() works", {
       "DESCRIPTION not tidy. Use `checklist::tidy_desc()`"
     )
   )
+
+  # upgrade to dev version number
+  desc::desc_bump_version(which = "dev", file = repo)
+  git_add(files = "DESCRIPTION", repo = repo)
+  gert::git_commit(message = "bump dev version", repo = repo)
+  expect_is(x <- check_description(repo), "Checklist")
+  expect_identical(
+    x$.__enclos_env__$private$errors$DESCRIPTION,
+    "Incorrect version tag format. Use `0.0` or `0.0.0`"
+  )
+
+  # upgrade to patch version number
+  desc::desc_bump_version(which = "patch", file = repo)
+  git_add(files = "DESCRIPTION", repo = repo)
+  gert::git_commit(message = "bump patch version", repo = repo)
+  expect_is(x <- check_description(repo), "Checklist")
+  expect_identical(
+    x$.__enclos_env__$private$errors$DESCRIPTION, character(0)
+  )
+
+  this_desc <- desc::description$new(
+    file = file.path(path, package, "DESCRIPTION")
+  )
+  this_desc$del_remotes("inbo/INBOmd")
+  this_desc$write()
+  git_add(files = "DESCRIPTION", repo = repo)
+  gert::git_commit(message = "remove remotes", repo = repo)
+  expect_is(x <- check_description(repo), "Checklist")
+  expect_identical(
+    x$.__enclos_env__$private$errors$DESCRIPTION,
+    "Package version not updated"
+  )
+
   gert::git_clone(
     url = file.path(path, package),
     path = file.path(path, "origin"),
     bare = TRUE, verbose = FALSE
   )
-  gert::git_remote_add(url = file.path(path, "origin"),
-                       name = "origin", repo = repo)
+  gert::git_remote_add(
+    url = file.path(path, "origin"), name = "origin", repo = repo
+  )
   git_fetch(remote = "origin", repo = repo, verbose = FALSE)
-  git_branch_create(branch = "junk",
-                          ref = "HEAD",
-                          checkout = TRUE,
-                          repo = repo)
+  git_branch_create(branch = "junk", ref = "HEAD", checkout = TRUE, repo = repo)
 
   expect_is(x <- check_description(file.path(path, package)), "Checklist")
   expect_identical(
