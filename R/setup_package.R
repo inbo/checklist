0bfdf2e4f9aa030887f0983d308f153e5d446496
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-10-24 11:07:18 +0200

    fix lintr

--- R/setup_package.R
+++ R/setup_package.R
@@ -91,7 +91,7 @@ setup_package <- function(path = ".",
       paste(
         "# %s 0.0.0", "",
         "* Added a `NEWS.md` file to track changes to the package.",
-        "* Add [`checklist`](https://inbo.github.io/checklist/) infrastructure.",
+      "* Add [`checklist`](https://inbo.github.io/checklist/) infrastructure.",
         sep = "\n"
       ),
       package
