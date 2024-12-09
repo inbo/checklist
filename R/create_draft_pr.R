22cd91ef864e69139fb97d48c8506e73d5015dd9
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-12-09 22:02:21 +0100

    ✨ Add create_draft_pr()
    Create a draft PR from the current branch to the main branch.

---
+++ R/create_draft_pr.R
@@ -0,0 +1,26 @@
+#' Create a draft pull request
+#'
+#' This function creates a draft pull request for the current version of the
+#' package.
+#'
+#' @param x The path to the package.
+#' @importFrom desc desc
+#' @importFrom gert git_info git_remote_info
+#' @importFrom gh gh
+#' @export
+#' @family package
+create_draft_pr <- function(x = ".") {
+  x <- read_checklist(x)
+  stopifnot("Current version only relevant for R packages" = x$package)
+  git_info <- git_info(repo = x$get_path)
+  remote <- git_remote_info(remote = git_info$remote, repo = x$get_path)
+  owner <- gsub("git@github.com:(.+)/(.+).git", "\\1", remote$url)
+  repo <- gsub("git@github.com:(.+)/(.+).git", "\\2", remote$url)
+  desc(x$get_path)$get_version() |>
+    as.character() -> version
+  gh(
+    "POST /repos/{owner}/{repo}/pulls", owner = owner, repo = repo,
+    head = basename(git_info$head), base = basename(remote$head),
+    title = sprintf(":bookmark:Version %s", version), draft = TRUE
+  )
+}
