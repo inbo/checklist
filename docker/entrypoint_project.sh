44c33fd4d4e3e06c1ab92bf0c27a732980b67106
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2025-02-26 15:57:04 +0100

    👷 Check for packages tries to install missing dependencies on the fly

--- docker/entrypoint_project.sh
+++ docker/entrypoint_project.sh
@@ -11,6 +11,7 @@ if [ -n "$INPUT_CRAN" ]; then
   CRAN='remotes::install_cran('${INPUT_CRAN}', upgrade = "always")'
   Rscript --no-save --no-restore -e "$CRAN"
 fi
+Rscript --no-save --no-restore -e 'install.packages(checklist:::list_missing_packages())'
 Rscript --no-save --no-restore -e 'checklist::check_project("check/'$INPUT_PATH'", fail = TRUE, quiet = FALSE)'
 if [ $? -ne 0 ]; then
   echo '\nThe project failed some checks. Please check the error message above.\n';
