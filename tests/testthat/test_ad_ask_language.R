de9c5450af16d8cdadc64fa1e2a2415ac0a5e903
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2025-11-15 13:45:18 +0100

    🐛 Fix failing test on non checklist infrastructure

--- tests/testthat/test_ad_ask_language.R
+++ tests/testthat/test_ad_ask_language.R
@@ -1,7 +1,6 @@
 library(mockery)
 test_that("ask_language", {
-  cache_org("https://github.com/inbo", config_folder = config_dir)
-  org <- org_list_from_url("https://github.com/inbo/checklist.git")
+  org <- cache_org("https://github.com/inbo", config_folder = config_dir)
   expect_identical(ask_language(org), "nl-BE")
   stub(ask_language, "menu_first", 5)
   stub(ask_language, "readline", mock("", "nl-NL"))
