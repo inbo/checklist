ca9f200ab870c9fb26be608409e6e42bfbb4c383
Ward Langeraert <ward.langeraert@inbo.be>
2026-02-17 14:32:20 +0100

    revert to original

--- tests/testthat/test_aa_ask_ror.R
+++ tests/testthat/test_aa_ask_ror.R
@@ -5,7 +5,7 @@ test_that("ask_ror", {
   stub(
     ask_ror,
     "readline",
-    mock("https://ror.org", "https://ror.org/00j54wy13", "https://ror.org/00j54wy13")
+    mock("https://ror.org", "00j54wy13", "https://ror.org/00j54wy13")
   )
   expect_warning(result <- ask_ror("What ROR"), "`ror` must be in ")
   expect_identical(result, "https://ror.org/00j54wy13")
