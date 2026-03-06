e16673588d12fa737fe73877c14ea5880e1af235
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-03-06 11:08:29 +0100

    🚨 Fix linter

--- R/fix_duplicate_git_config.R
+++ R/fix_duplicate_git_config.R
@@ -8,10 +8,13 @@ fix_duplicate_git_config <- function(repo = ".") {
   grepl("^branch", problem) |>
     setNames(sprintf("unhandled duplicate in git config: `%s`", problem)) |>
     stopifnot()
-  file.path(repo, ".git", "config") |>
-    readLines() -> local_config
+  file.path(repo, ".git", "config") |> readLines() -> local_config
   branch_position <- grep("\\[branch \"(.*)\"\\]", local_config)
-  branch_name <- gsub("\\[branch \"(.*)\"\\]", "\\1", local_config[branch_position])
+  branch_name <- gsub(
+    "\\[branch \"(.*)\"\\]",
+    "\\1",
+    local_config[branch_position]
+  )
   paste0("branch.", branch_name, ".") |>
     vapply(grepl, logical(1), x = problem) |>
     which() -> matching_branch
@@ -21,7 +24,7 @@ fix_duplicate_git_config <- function(repo = ".") {
     keep_end <- character(0)
     tail(local_config, -branch_position[1]) -> to_alter
   } else {
-    keep_end <- tail(local_config, 1-branch_position[2])
+    keep_end <- tail(local_config, 1 - branch_position[2])
     head(local_config, branch_position[2] - 1) |>
       tail(-branch_position[1]) -> to_alter
   }
