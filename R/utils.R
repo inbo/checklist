826c95339e0d9b4078a02c4c47042e37fc9d50d7
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-05-20 14:38:55 +0200

    🔥 Move validate_email() to the citeme package

--- R/utils.R
+++ R/utils.R
@@ -53,31 +53,6 @@ on_failure(is_workdir_clean) <- function(call, env) {
   "Working directory is not clean. Please commit or stash changes first."
 }
 
-#' Check if a vector contains valid email
-#'
-#' It only checks the format of the text, not if the email address exists.
-#' @param email A vector with email addresses.
-#' @return A logical vector.
-#' @export
-#' @importFrom assertthat assert_that
-#' @family utils
-validate_email <- function(email) {
-  assert_that(is.character(email))
-  # expression taken from https://emailregex.com/
-  grepl(
-    paste0(
-      "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"",
-      "(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|",
-      "\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*",
-      "[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|",
-      "2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]",
-      "[0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a",
-      "\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])"
-    ),
-    tolower(email)
-  )
-}
-
 checklist_extract <- function(x, name = "value", prefix = rep("", length(x))) {
   paste0(prefix, vapply(x, `[[`, character(1), name))
 }
